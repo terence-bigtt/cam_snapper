cf9217685db2f27589507c93a7ffe814a9b8d74f
terence <terence.delsate>
2017-12-04 16:02:08 +0100

    async scheduling in progress

--- src/scheduler/scheduler.py
+++ src/scheduler/scheduler.py
@@ -1,178 +1,78 @@
 import uuid
 import threading
 import concurrent.futures
-
-class Scheduler:
-    def __init__(self, run=True):
-        self.scheduled = {}
-        self.history = {}
-        self.running = run
-        self.paused = {}
-        self.stopped = {}
-
-    def schedule(self, task):
-        task_id = str(uuid.uuid4())
-        self.scheduled.update({task_id: task})
-        if self.running:
-            task.start()
-        return task_id
-
-    def get_tasks(self, task_id=None):
-        tasks = self.scheduled.values()
-        if task_id:
-            tasks = [t for t in [self.scheduled.get(task_id)] if t is not None]
-        return tasks
-
-    def _get_bool_dict(self, taskid, bval):
-        if taskid:
-            return {taskid: True}
-        else:
-            return {tid: True for tid in self.scheduled.keys()}
-
-    def pause(self, taskid=None):
-        self.paused.update(self._get_bool_dict(taskid, True))
-        for t in self.get_tasks(taskid):
-            t.pause()
-
-    def resume(self, taskid=None):
-        self.paused.update(self._get_bool_dict(taskid, False))
-        for t in self.get_tasks(taskid):
-            t.resume()
-
-    def stop(self, taskid=None):
-        self.stopped.update(self._get_bool_dict(taskid, True))
-        for t in self.get_tasks(taskid):
-            t.cancel()
-        for t in self.get_tasks(taskid):
-            t.join()
-        self._clean_schedule()
-
-    def _clean_schedule(self):
-        self.history.update({tid: t for tid, t in self.scheduled.items() if not t.is_alive()})
-        self.scheduled = {tid: t for tid, t in self.scheduled.items() if tid not in self.history.keys()}
-
-    def run(self, taskid=None):
-        self.running = True
-        for t in self.get_tasks(taskid):
-            try:
-                t.start()
-            except Exception as e:
-                print(e.args)
-                pass
-
-
-class ScheduleLoop(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self):
-
-
-class SchedulerAsync(threading.Thread):
-    def __init__(self, run=True):
-        self.scheduled = {}
-        self.history = {}
-        self.running = run
-        self.paused = {}
-        self.stopped = {}
-
-
-    def schedule(self, task):
-        task_id = str(uuid.uuid4())
-        self.scheduled.update({task_id: task})
-        if self.running:
-            task.start()
-        return task_id
-
-
 import time
 
 
-class ScheduledTaskAsync():
-    def __init__(self, cmd, every=None, timeout=None, max_error_tol=10):
-        self.last_ran = None
-        self.started_at = None
-        self.timeout = timeout
-        self.max_error_tol = max_error_tol
-        self.cmd = cmd
-        self.errors = []
-        self.last_result = None
-        self.next_run = timeout or 0
-        self.every = every
-        self.cancelled = False
-        self.paused = False
-        threading.Thread.__init__(self)
+class Scheduler(object):
+    def __init__(self, max_workers=None, thread_name_prefix="", running=True):
+        self.scheduled = {}
+        self.history = {}
+        self.results = {}
+        self._running = running
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers,
+                                                              thread_name_prefix=thread_name_prefix)
+        self.thread = threading.Thread(target=self._run)
+        self._lock = False
+        if running:
+            self.start()
 
-    def pause(self):
-        self.paused = True
+    def schedule(self, task):
+        taskid = str(uuid.uuid4())
+        task.set_executor(self.executor)
+        self._lock = True
+        while self._loopstart: pass
+        self.scheduled.update({taskid: task})
+        self._lock = False
+        return taskid
 
-    def resume(self):
-        self.paused = False
+    def pause_all(self):
+        for tid, task in self.scheduled.items():
+            task.pause()
 
-    def cancel(self):
-        self.cancelled = True
+    def pause(self, taskid):
+        task = self.scheduled.get(taskid)
+        if task:
+            task.pause()
 
-    def _initialize(self):
-        self.started_at = time.time()
+    def resume(self, taskid):
+        task = self.scheduled.get(taskid)
+        if task:
+            task.resume()
 
-    def _should_run_in(self):
-        if self.cancelled:
-            return
-        now = time.time()
-        initialized_since = now - (self.last_ran or self.started_at)
-        if self.next_run is None:
-            return
-        return self.next_run - (initialized_since or 0)
+    def resume_all(self):
+        for tid, task in self.scheduled.items():
+            task.resume()
 
-    def run(self):
-        starting = yield
-        self._initialize()
-        while True:
-            request = yield self._should_run_in() >0
-            if request:
-                if request == 'status' :
-                    yield {'paused': self.paused, 'cancelled': self.cancelled}
+    def cancel(self, taskid):
+        task = self.scheduled.pop(taskid, None)
+        if task:
+            task.cancel()
+            self.history.update({taskid: task})
 
-        self._initialize()
-        goon = True
-        runner = self._runner()
-        while goon:
-            goon = next(runner)
+    def stop(self):
+        self._running = False
 
-    def _runner(self):
-        goon = True
-        received = None
-        while True:
-            if received is not None:
-                goon = received
-            should_run_in = self._should_run_in()
-            if should_run_in is None or not goon or self.cancelled:
-                yield False
-                break
+    def start(self):
+        try:
+            if not self.thread.isAlive():
+                self.running = True
+                self.thread.start()
 
-            if self.paused or should_run_in > 0:
-                dorun = False
-            else:
-                dorun = True
+        except RuntimeError:
+            self.thread = threading.Thread(target=self._run)
+            self._running = True
+            self.thread.start()
 
-            if dorun:
-                try:
-                    self.last_result = self.cmd()
-                    self.last_ran = time.time()
-                    self.next_run = None
-                    if self.every is not None:
-                        self.next_run = self.every
-
-                except Exception as e:
-                    print(e)
-                    print("exception in run", len(self.errors), self.max_error_tol)
-                    self.errors.append(e)
-                    if len(self.errors) >= self.max_error_tol:
-                        self.cancel()
-                        yield False
-                        break
-
-            received = yield goon
-
-
-
+    def _run(self):
+        while self._running:
+            if not self._lock:
+                self._loopstart = True
+                runningschedule = {k: v for k, v in self.scheduled.items()}
+                self._loopstart = False
+                for tid, task in runningschedule.items():
+                    runningtask = next(task)
+                    if runningtask.isexecuting:
+                        self.results.update({tid: runningtask})
+                    if runningtask.done:
+                        self.history.update({tid: self.scheduled.pop(tid)})
--- src/scheduler/task.py
+++ src/scheduler/task.py
@@ -1,83 +1,126 @@
-import time, threading
+
+import time
+
+class RunningTask(object):
+    def __init__(self, future=None, will_run_in=None, paused=False, cancelled=False, done=False):
+        self.future = future
+        self.will_run_in = will_run_in
+        self.ispaused = paused
+        self.iscancelled = cancelled
+        self.done = done
+
+    @property
+    def isexecuting(self):
+        return self.future is not None
+
+    @property
+    def iswaiting(self):
+        return self.will_run_in is not None
 
 
-class ScheduledTask(threading.Thread):
-    def __init__(self, cmd, every=None, timeout=None, max_error_tol=10):
-        self.last_ran = None
-        self.started_at = None
+class TaskException(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class Task(object):
+    def __init__(self, cmd, *args, **kwargs):
+        kw = {k: v for k, v in kwargs.items()}
+        every = kw.pop("every", None)
+        timeout = kw.pop("timeout", None)
+        max_error_tol = kw.pop("max_error_tol", 10)
+        self._last_ran = None
+        self._started_at = None
         self.timeout = timeout
         self.max_error_tol = max_error_tol
         self.cmd = cmd
+        self.args = args
+        self.kwargs = kw
         self.errors = []
         self.last_result = None
-        self.next_run = timeout or 0
+        self._next_run = timeout or 0
         self.every = every
         self.cancelled = False
         self.paused = False
-        threading.Thread.__init__(self)
+        self._paused_at = None
+        self._generator = self.run()
+        self.executor = None
 
-    def reset(self):
-        return ScheduledTask(self.cmd, self.every, self.timeout, self.max_error_tol)
+    def set_executor(self, executor):
+        self.executor = executor
+
+    def __next__(self):
+        return next(self._generator)
 
     def pause(self):
         self.paused = True
+        self._paused_at = time.time()
 
     def resume(self):
         self.paused = False
+        if self._paused_at:
+            timesincepause = time.time() - self._paused_at
+            self._next_run += timesincepause
+            self._paused_at = None
 
     def cancel(self):
         self.cancelled = True
 
-    def initialize(self):
-        self.started_at = time.time()
+    def _initialize(self):
+        self._started_at = time.time()
 
-    def should_run_in(self):
+    def _should_run_in(self):
         if self.cancelled:
             return
         now = time.time()
-        initialized_since = now - (self.last_ran or self.started_at)
-        if self.next_run is None:
+        time_since_last_run = now - (self._last_ran or self._started_at)
+        if self._next_run is None:
             return
-        return self.next_run - (initialized_since or 0)
+        return self._next_run - (time_since_last_run or 0)
 
     def run(self):
-        self.initialize()
-        goon = True
-        runner = self._runner()
-        while goon:
-            goon = next(runner)
-
-    def _runner(self):
-        goon = True
-        received = None
+        self._initialize()
+        yield RunningTask()
         while True:
-            if received is not None:
-                goon = received
-            should_run_in = self.should_run_in()
-            if should_run_in is None or not goon or self.cancelled:
-                yield False
-                break
+            if self.executor is None:
+                raise TaskException("Executor not defined. Use set_executor(executor).")
 
-            if self.paused or should_run_in > 0:
-                dorun = False
+            if self.cancelled:
+                result = RunningTask(cancelled=True)
+
+            elif self.paused:
+                result = RunningTask(paused=True)
             else:
-                dorun = True
+                willrunin = self._should_run_in()
+                if not willrunin:
+                    self.cancel()
+                    result = RunningTask(done=True, cancelled=True)
+                else:
+                    shouldrun = willrunin < 0
+                    if shouldrun:
+                        future = self.executor.submit(self._executecmd)
+                        next_run = self.setup_next_run()
+                        result = RunningTask(future=future, will_run_in=next_run)
+                    else:
+                        result = RunningTask(will_run_in=willrunin, paused=self.paused, cancelled=self.cancelled)
+            yield result
 
-            if dorun:
-                try:
-                    self.last_result = self.cmd()
-                    self.last_ran = time.time()
-                    self.next_run = None
-                    if self.every is not None:
-                        self.next_run = self.every
+    def setup_next_run(self):
+        self._last_ran = time.time()
+        self._next_run = None
+        if self.every is not None:
+            self._next_run = self.every
+        return self._next_run
 
-                except Exception as e:
-                    print(e)
-                    print("exception in run", len(self.errors), self.max_error_tol)
-                    self.errors.append(e)
-                    if len(self.errors) >= self.max_error_tol:
-                        self.cancel()
-                        yield False
-                        break
+    def _executecmd(self):
+        try:
+            return self.cmd(*self.args, **self.kwargs)
 
-            received = yield goon
+        except Exception as e:
+            print(e)
+            print("exception in run", len(self.errors), self.max_error_tol)
+            self.errors.append(e)
+            if len(self.errors) >= self.max_error_tol:
+                self.cancel()
+
+
